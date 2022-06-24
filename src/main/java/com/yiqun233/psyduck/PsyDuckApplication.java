e48e280ba36ae22d68c4db65f0c17492d4527a40
Qun Q Yi <qun.q.yi@cn.pwc.com>
2022-06-24 15:11:42 +0800

    Initial commit

---
+++ src/main/java/com/yiqun233/psyduck/PsyDuckApplication.java
@@ -0,0 +1,13 @@
+package com.yiqun233.psyduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyDuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsyDuckApplication.class, args);
+    }
+
+}
